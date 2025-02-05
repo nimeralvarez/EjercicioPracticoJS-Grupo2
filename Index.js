04b6ca95069b39e1f01e64314e586013eb743ad1
WillyAguilar09 <jwsellbycar@gmail.com>
2025-02-04 21:58:26 -0600

    Creating Function prime numbers

--- Index.js
+++ Index.js
@@ -1,3 +1,19 @@
-function numerosPrimes(){
+function numerosPrimos(n) {
+    let mensaje;
+    let divisores = 0;
+    
+    for (let i = 1; i < n; i++) {
+        let reciduo = n % i;
+        if (reciduo == 0) {
+            divisores++;
+        }
+    }
+    if (divisores = 2) {
+        return "El numero" + n + "es primo";
 
+    } else {
+        return "El numero" + n + "no es primo";
+
+    }
+    
 }
